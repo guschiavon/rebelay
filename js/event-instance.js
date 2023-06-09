cea7ea88c7f832352e43746cbabc14bc4aaa1a98
Gus Schiavon <hello@itsgus.dev>
2023-06-09 17:31:58 -0300

    Refactor web component code to allow for modularity

--- js/event-instance.js
+++ js/event-instance.js
@@ -1,4 +1,4 @@
-import eventListing from "./components/event-listing.component.js";
+import eventListing from "../js/components/event-listing.component.js";
 const createEventInstance = (event) => {
   const eventsEl = document.getElementById('events');  
   let { startDate, endDate, type, title, summary, url, location, organizer, addToCal } = event;                               
@@ -7,7 +7,7 @@ const createEventInstance = (event) => {
   eventInstance.classList.add('event-instance');
   eventInstance.classList.add('bg-gray-100');
   eventInstance.innerHTML = eventListing(type,url,title,startDate,endDate,location,summary,addToCal)
-   eventsEl.appendChild(eventInstance);
+  eventsEl.appendChild(eventInstance);
 }
 
 export default createEventInstance;
