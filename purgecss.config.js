d43af38d46c069f452b672c6db51a162401307ad
Gus Schiavon <hello@itsgus.dev>
2023-06-09 18:09:26 -0300

    add favicon

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  content: ["./_site/**/*.html"],
+  content: [
+      "./_includes/**/*.html",
+      "./_layouts/**/*.html",
+      "./_posts/*.md",
+      "./*.html",
+      "./js/*.js",
+      "./js/components/*.component.js"
+    ],
   css: ["./_site/style/*.css"],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []  
 };
